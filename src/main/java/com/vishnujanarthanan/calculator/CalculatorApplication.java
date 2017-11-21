c2413dbd2e8d16722c1c114d9d8498ddff4f3324
vishnu <vishnuselva@gmail.com>
2017-11-21 03:49:58 -0500

    add unit testing

--- src/main/java/com/vishnujanarthanan/calculator/CalculatorApplication.java
+++ src/main/java/com/vishnujanarthanan/calculator/CalculatorApplication.java
@@ -1,5 +1,5 @@
 package com.vishnujanarthanan.calculator;
-
+import org.springframework.stereotype.Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
